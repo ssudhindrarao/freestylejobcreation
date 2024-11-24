f48b5194782bd4f4fefe4e11e0040f42d3de9cd0
Sudhindra <ssudhindrarao@gmail.com>
2024-11-24 21:36:11 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
